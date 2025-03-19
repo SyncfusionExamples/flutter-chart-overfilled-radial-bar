a7e70e405a8d41e9ad492910e9770af55f1804e1
AswiniDileep <aswini.s@syncfusion.com>
2025-03-19 15:32:33 +0530

    FLUT-922222-[others]: Updated test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:charts/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const RadialBarCustomized());
+    await tester.pumpWidget(const OverfilledRadialBar());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
